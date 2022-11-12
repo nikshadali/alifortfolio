720493a2e82cf63c32184d6765cb5e8e6914e29d
nikshad <nikali92100@gmail.com>
2022-11-12 13:02:41 +0500

    change button

--- components/Header.js
+++ components/Header.js
@@ -2,27 +2,31 @@ import Typewriter from 'typewriter-effect';
 import Image from 'next/image';
 import headr8 from '../public/image/header8.webp'
 import Link from 'next/link';
-import  jsPDF  from "jspdf";
+import { useState } from 'react';
+
 
 
  const Header = () => {
-  
+  const myCv = "/pdf/myCv.pdf"
+ const [myCV, setMyCV] = useState()
 
   // Function will execute on click of button
     const onButtonClick = () => {
-      const myCv = "/pdf/myCv.pdf"
-   // using Java Script method to get PDF file
-   fetch(myCv).then(response => {
-   response.blob().then(blob => {
-   // Creating new object of PDF file
-    const fileURL = window.URL.createObjectURL(blob);
-    // Setting various property values
-    let alink = document.createElement('a');
-    alink.href = fileURL;
-    alink.download = myCv;
-    alink.click();
-   })
-   })
+    
+  //  // using Java Script method to get PDF file
+  //  fetch(myCv).then(response => {
+  //  response.blob().then(blob => {
+  //  // Creating new object of PDF file
+  //   const fileURL = window.URL.createObjectURL(blob);
+  //   // Setting various property values
+  //   let alink = document.createElement('a');
+  //   alink.href = fileURL;
+  //   alink.download = myCv;
+  //   alink.click();
+  //  })
+  //  })
+      setMyCV(myCV)
+
    }
  
 
@@ -50,7 +54,7 @@ import  jsPDF  from "jspdf";
             Skilled and motivated web developer looking for a challenging role in a reputable organization to utilize my technical, database, programming, and management skills for the growth of the organization as well as to enhance my knowledge about new and emerging trends in the IT sector.
             </p>
             <div className="flex items-center">
-            <button className="inline-flex items-center justify-center h-12 px-10 mr-6 font-medium tracking-wide text-white transition duration-500 rounded-3xl shadow-md bg-purple-700 hover:-translate-y-2 hover:drop-shadow-2xl focus:shadow-outline focus:outline-none" onClick={onButtonClick} > Download CV </button>
+            <Link href={myCv} className="inline-flex items-center justify-center h-12 px-10 mr-6 font-medium tracking-wide text-white transition duration-500 rounded-3xl shadow-md bg-purple-700 hover:-translate-y-2 hover:drop-shadow-2xl focus:shadow-outline focus:outline-none" onClick={onButtonClick} > Download CV </Link>
            
        
             </div>
